820b095093447859754cc57a7e39d3c1d172f7d3
Susheel Thapa <susheel.thapa@outlook.com>
2022-05-25 17:24:17 +0545

    Simple main file
    It consist of basic SDL code to render the window.
    However, error are not handle.
    It is used to make sure it work in both the OS.
    Currently it works in Linux.
    Later on we will check in Window and add necessary changes.

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#ifdef __linux__
+#include <SDL2/SDL.h>
+
+#elif _WIN32
+// Header files to be used by window OS
+
+#endif
+
+int main(int argc, char *args[])
+{
+    SDL_Window *window = nullptr;
+    SDL_Surface *screen_surface = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_SHOWN);
+
+    screen_surface = SDL_GetWindowSurface(window);
+
+    SDL_FillRect(screen_surface, nullptr, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
+}
