fde02b81804e89e8b43ed662f5dd3d749c4cefed
Susheel Thapa <susheel.thapa@outlook.com>
2022-06-08 02:50:33 +0545

    Script file to run program in Linux
    It contain the terminal command to compile, run and delete the executable file.

---
+++ run.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+# Useful variables
+file_to_compile="*.cpp"
+output_filename="main"
+compiler_flags="-w"
+SDL_library="-lSDL2"
+
+# print the command to compile files with SDL
+echo "g++ $file_to_compile $compiler_flags $SDL_library -o $output_filename"
+
+# Compiling command
+g++ $file_to_compile $compiler_flags $SDL_library -o $output_filename
+./$output_filename
+rm $output_filename
