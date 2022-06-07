a6ad13dd6ebf7b1d5b18b251f1f420ac6934dd4d
Susheel Thapa <susheel.thapa@outlook.com>
2022-06-08 02:55:33 +0545

    A sample program to demonstrate the window header file

--- main.cpp
+++ main.cpp
@@ -1,34 +1,22 @@
-#include <iostream>
+#include "window.hpp"
 
-#ifdef __linux__
-#include <SDL2/SDL.h>
-
-#elif _WIN32
-#include <SDL.h>
-
-
-#endif
-
-int main(int argc, char *args[])
+int main()
 {
-    SDL_Window *window = nullptr;
-    SDL_Surface *screen_surface = nullptr;
+    // An object of window class
+    Window window("SDL");
 
-    SDL_Init(SDL_INIT_VIDEO);
+    /*Game Loop*/
+    while (!window.isWindowClosed())
+    {
+        // Handle the events
+        window.handleEvent();
 
-    window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_SHOWN);
+        // Clear the window
+        window.clear({0, 255, 255, 255});
 
-    screen_surface = SDL_GetWindowSurface(window);
-
-    SDL_FillRect(screen_surface, nullptr, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
-
-    SDL_UpdateWindowSurface(window);
-
-    SDL_Delay(2000);
-
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
+        // Present the window.
+        window.present();
+    }
 
     return 0;
 }
