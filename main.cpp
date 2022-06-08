66191102938b50c9ecd48c4c97655527732e0aea
Susheel Thapa <susheel.thapa@outlook.com>
2022-06-08 09:51:05 +0545

    Update code in main.cpp, run.sh and added neccesary images
    main.cpp has been update to demonstrate Texture header file
    run.sh is update with some new linker libraries as new SDL header file are include in window.hpp
    And for demo it make use of image which has been added.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include "window.hpp"
+#include "texture.hpp"
 
 int main()
 {
-    // An object of window class
+    // An object of window class with 400 * 400 dimension
     Window window("SDL");
+
+    // An instance of texture class
+    Texture texture;
+
+    // Load images in the Texture class object
+    texture.loadFromFile(window, "resources/images/sdl-logo.png");
+
+    // Parameter to been while rendering the texture
+    SDL_Rect render_quad = {0, 0, texture.getWidth(), texture.getHeight()};
+    SDL_Rect render_rect = {0, 0, render_quad.w / 2, render_quad.h};
+    SDL_Point center_of_rotation = {window.getWidth() / 2, window.getHeight() / 2};
 
     /*Game Loop*/
     while (!window.isWindowClosed())
@@ -13,6 +25,14 @@ int main()
 
         // Clear the window
         window.clear({0, 255, 255, 255});
+
+        /* Display the image we have  loaded over the screen of window we have created*/
+        // Out of below render method run one at a time so that you can know what each argument does
+        texture.render(window, 0, 0, nullptr, nullptr);
+        texture.render(window, 0, 0, nullptr, &render_quad);
+        texture.render(window, 0, 0, &render_rect, &render_quad);
+        texture.render(window, 0, 0, nullptr, &render_quad, 60, &center_of_rotation, SDL_FLIP_HORIZONTAL);
+        texture.render(window, 0, 0, nullptr, &render_quad, -49, &center_of_rotation, SDL_FLIP_VERTICAL);
 
         // Present the window.
         window.present();
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 file_to_compile="*.cpp"
 output_filename="main"
 compiler_flags="-w"
-SDL_library="-lSDL2"
+SDL_library="-lSDL2 -lSDL2_image -lSDL2_ttf"
 
 # print the command to compile files with SDL
 echo "g++ $file_to_compile $compiler_flags $SDL_library -o $output_filename"
@@ -12,4 +12,6 @@ echo "g++ $file_to_compile $compiler_flags $SDL_library -o $output_filename"
 # Compiling command
 g++ $file_to_compile $compiler_flags $SDL_library -o $output_filename
 ./$output_filename
+
+# Remove the executable file
 rm $output_filename
