5c102b4bd59eefccbc48ab48f1c3c5fe05bbf78b
Susheel Thapa <susheel.thapa@outlook.com>
2022-06-11 13:11:04 +0545

    Updated main.cpp to demonstrate Shape class we have created and fix the command display by run.sh

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include "window.hpp"
 #include "texture.hpp"
+#include "line.hpp"
+#include "triangle.hpp"
+#include "rectangle.hpp"
 
 int main(int argc, char *argv[])
 {
     // An object of window class with 400 * 400 dimension
     Window window("SDL");
 
-    // An instance of texture class
-    Texture texture;
+    // An instance of shape class
+    Line l1;
+    Triangle t1;
+    Rectangle r1({100, 100}, 50, 50);
 
-    // Load images in the Texture class object
-    texture.loadFromFile(window, "resources/images/sdl-logo.png");
+    l1.setColor(255, 255, 255, 255);
+    l1.setPoints(100, 200, 50, 350);
 
-    // Parameter to been while rendering the texture
-    SDL_Rect render_quad = {0, 0, texture.getWidth(), texture.getHeight()};
-    SDL_Rect render_rect = {0, 0, render_quad.w / 2, render_quad.h};
-    SDL_Point center_of_rotation = {window.getWidth() / 2, window.getHeight() / 2};
+    t1.setColor(255, 0, 0, 255);
+    t1.setPoints(0, 0, 100, 300, 400, 390);
 
     /*Game Loop*/
     while (!window.isWindowClosed())
@@ -24,15 +27,11 @@ int main(int argc, char *argv[])
         window.handleEvent();
 
         // Clear the window
-        window.clear({0, 255, 255, 255});
+        window.clear({0, 0, 0, 0});
 
-        /* Display the image we have  loaded over the screen of window we have created*/
-        // Out of below render method run one at a time so that you can know what each argument does
-        texture.render(window, 0, 0, nullptr, nullptr);
-        texture.render(window, 0, 0, nullptr, &render_quad);
-        texture.render(window, 0, 0, &render_rect, &render_quad);
-        texture.render(window, 0, 0, nullptr, &render_quad, 60, &center_of_rotation, SDL_FLIP_HORIZONTAL);
-        texture.render(window, 0, 0, nullptr, &render_quad, -49, &center_of_rotation, SDL_FLIP_VERTICAL);
+        l1.render(window);
+        t1.render(window);
+        r1.render(window);
 
         // Present the window.
         window.present();
